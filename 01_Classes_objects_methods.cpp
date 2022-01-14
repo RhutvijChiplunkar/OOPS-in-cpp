9a534843baa6f16de2a08e7c30550fcbe9c3301f
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-01-14 20:12:27 +0530

    Classes and objects in cpp along with constructors and class methods

---
+++ 01_Classes_objects_methods.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//creation of a class
+class Student{
+    //Note: All the members are private by default
+public:
+    string first_name,last_name;
+    int age;
+    vector<string> skills;
+
+    //constructor -->name same as that of class, invoked when object is created, NO RETURN TYPE
+
+    Student(){
+        cout<<"Student class"<<endl;
+    }
+    //This is parameterized constructor
+    Student(string name,string surname){
+        first_name=name;
+        last_name=surname;
+    }
+
+    //Class methods
+    void printInfo(){
+        cout<<"\n==================="<<endl;
+        cout<<"Student name::"<< first_name<<" "<<last_name<<endl;
+        cout<<"Age::"<<age<<endl;
+        cout<<"Skills of the student are::"<<endl;
+        for(auto i:skills){
+            cout<<i<<endl;
+        }
+        cout<<"==================="<<endl;
+    }
+
+};
+
+int main(){
+    //Creation of object of a class
+    Student s1("Rhutvij","Chiplunkar");
+
+    //Access all the attributes and methods by using a .
+    s1.age=21;  
+    s1.skills.push_back("C++");
+    s1.skills.push_back("OOPS");
+    s1.printInfo();
+
+    Student s2("Soham","Chiplunkar");
+    s2.age=24;  
+    s2.skills.push_back("Forensics");
+    s2.skills.push_back("Security");
+    s2.printInfo();
+
+    //This is the array of objects
+    Student St[5];
+}
